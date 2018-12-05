9c1e0ac464f201ddea50223fdc4facf60ab66f2b
Quang Nguyen <nguyenq@MBP.local>
2018-12-05 23:42:27 +1100

    One more try

--- ux/auschamps/service-worker.js
+++ ux/auschamps/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9aa932c5a3230c94be439acb4356fe62.js"
+  "/ux/auschamps/precache-manifest.cc67ecea471c33cdd19bd3cd3547b362.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/ux/auschamps/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
