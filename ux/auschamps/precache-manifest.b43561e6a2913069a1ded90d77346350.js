af53f53bbbfde65e89439259df00661a0b484997
Quang Nguyen <nguyenq@MBP.local>
2018-12-05 22:30:00 +1100

    Added auschamps

---
+++ ux/auschamps/precache-manifest.b43561e6a2913069a1ded90d77346350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba575fb48732689b8d7a",
+    "url": "/static/js/main.ba575fb4.chunk.js"
+  },
+  {
+    "revision": "0cabbfd55bc532e54f37",
+    "url": "/static/js/1.0cabbfd5.chunk.js"
+  },
+  {
+    "revision": "ba575fb48732689b8d7a",
+    "url": "/static/css/main.5d2ba469.chunk.css"
+  },
+  {
+    "revision": "0cabbfd55bc532e54f37",
+    "url": "/static/css/1.c8ef90fb.chunk.css"
+  },
+  {
+    "revision": "122fd74d66e94f759cda5133e923aa8c",
+    "url": "/index.html"
+  }
+];
