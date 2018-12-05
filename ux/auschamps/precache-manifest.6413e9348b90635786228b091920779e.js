d3a66abd8de057af18f016f1d27670617b38bf9d
Quang Nguyen <nguyenq@MBP.local>
2018-12-05 23:34:02 +1100

    Fixed relative path

---
+++ ux/auschamps/precache-manifest.6413e9348b90635786228b091920779e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a38f412b6b83466bcf3d",
+    "url": "./static/js/main.a38f412b.chunk.js"
+  },
+  {
+    "revision": "4a17f53555cd7b2113cd",
+    "url": "./static/js/1.4a17f535.chunk.js"
+  },
+  {
+    "revision": "a38f412b6b83466bcf3d",
+    "url": "./static/css/main.5d2ba469.chunk.css"
+  },
+  {
+    "revision": "4a17f53555cd7b2113cd",
+    "url": "./static/css/1.c8ef90fb.chunk.css"
+  },
+  {
+    "revision": "8a8e6b31395aa12fbbe2c7626e60728f",
+    "url": "./index.html"
+  }
+];
--- ux/auschamps/service-worker.js
+++ ux/auschamps/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b43561e6a2913069a1ded90d77346350.js"
+  "./precache-manifest.6413e9348b90635786228b091920779e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
