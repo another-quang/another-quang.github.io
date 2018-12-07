678616cefb31a11216c144b60cb45fd1860273eb
Quang <nguyenq@wks-admins-MacBook-Pro-3.local>
2018-12-07 11:33:00 +1100

    ff2f

---
+++ build/precache-manifest.70bdd94736633faa826eba651087e4b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04254e3364f9ed4940ac",
+    "url": "/static/css/main.43c79e28.chunk.css"
+  },
+  {
+    "revision": "04254e3364f9ed4940ac",
+    "url": "/static/js/main.04254e33.chunk.js"
+  },
+  {
+    "revision": "60fc48c400e96456aa56",
+    "url": "/static/js/1.60fc48c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9cb6ce66aca08a00d6b106474750924",
+    "url": "/index.html"
+  }
+];
